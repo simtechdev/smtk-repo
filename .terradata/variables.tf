edac8850af864aa097bfd42b759aba19ed7ed22e
Gleb Goncharov <ggoncharov@simtechdev.com>
2016-04-21 05:08:49 -0400

    TerraFarm data sample added.

---
+++ .terradata/variables.tf
@@ -0,0 +1,23 @@
+variable key {
+  default = ""
+}
+
+variable fingerprint {
+  default = ""
+}
+
+variable auth {
+  default = ""
+}
+
+variable token {
+  default = ""
+}
+
+variable region {
+  default = "fra1"
+}
+
+variable node_size {
+  default = "16gb"
+}
